758020ea107b00d8d0c4a772a71ad80eabbf9c70
Alex andr Bodachevskiy <alex.bodachevskiy@gmail.com>
2022-03-22 19:24:25 +0200

    add LS task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,32 @@ import { nanoid } from 'nanoid';
 class App extends Component {
   state = {
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  
     ],
     filter:""
+  }
+
+
+  componentDidMount() { 
+
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    const nextContacts =  this.state.contacts;
+    const prevContacts = prevState.contacts;
+
+    if (nextContacts !== prevContacts) {
+
+      localStorage.setItem('contacts', JSON.stringify(nextContacts));
+    }
+
+    
   }
 
   addContacts= ({name, number}) => {
@@ -55,6 +75,7 @@ class App extends Component {
   render () {    
     const {filter} = this.state;
     const renderContacts = this.visibleContacts();
+    
  
     return (
       <div>
